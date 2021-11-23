55cb199c0b010798315fa9e8a3544813c72532a1
01000001-01001110 <48245017+01000001-01001110@users.noreply.github.com>
2021-11-23 15:52:43 -0500

    Update quser.ps1
    Changed $ComputerNames = (Get-ADComputer -Filter *).Names to $ComputerNames = (Get-ADComputer -Filter *).Name

--- quser.ps1
+++ quser.ps1
@@ -13,7 +13,7 @@ This is how I would do it.
 
 #Now I like that I get all the data, but we only need the names. 
 
-$ComputerNames = (Get-ADComputer -Filter *).Names
+$ComputerNames = (Get-ADComputer -Filter *).Name
 #This puts the entire command's output into the array $ComputerNames. We only need the names. This is how we do this. 
 
 #Now we need to go through this list. With a loop. I like foreach for this. 
